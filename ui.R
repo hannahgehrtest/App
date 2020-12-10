096a49c3d264abe2b2cedd8c969c748e7f3d9df6
Hannah Gehrtest <hannahgehrmann@hotmail.de>
2020-12-10 12:13:36 +0100

    error tabPanel

--- ui.R
+++ ui.R
@@ -11,37 +11,36 @@ ui <- fluidPage(
   
   theme = shinytheme("flatly"),
   
-  # caption
+  
   titlePanel("PGSF 2020"),
   
-  # layout
-  sidebarLayout(
+
+  tabsetPanel(
+    tabPanel(
+      "Tab", 
+    sidebarLayout(
     sidebarPanel(
-      
-      # legend
-      
-      
-      # select year
-      sliderInput(inputId = "start", 
-                  label = "Jahreszahl auswählen:",
-                  min = 1970,
-                  max = 1989,
-                  value = 1970),
       selectInput(inputId = "kreis",
                   label = "Kreis:",
                   c("Potsdam" = "Potsdam",
                     "Berlin" = "Berlin Ost")
                   )
-    
-      
-    ),
-    mainPanel(
-      tabsetPanel(
-        tabPanel("First Tab",
-                 plotOutput("shrink_plot_re")
-        ),
-        tabPanel("Second Tab")
-      )
-    )
-  )
-)
+                 ),
+    mainPanel(plotOutput("shrink_plot_re"))
+                 )
+             ),
+   tabPanel(
+    "Tab 2", 
+    sidebarLayout(
+      sidebarPanel(
+        sliderInput(inputId = "start", 
+                    label = "Jahreszahl auswählen:",
+                    min = 1970,
+                    max = 1989,
+                    value = 1970)),
+        mainPanel()
+       )
+     )
+   )
+ )
+
